885fd332c190ecd2d152b5b6b579ee319d982b71
Le Minh Duc <fmgduc79@gmail.com>
2024-04-20 03:34:01 +0700

    update

--- src/Physics/Vector2D.h
+++ src/Physics/Vector2D.h
@@ -48,4 +48,47 @@ class Transform
         inline void TranslateY(float y) { Y += y; }
         inline void Translate(Vector2D v) { X += v.X; Y += v.Y; }
 };
+class IObject
+{
+    public:
+      virtual void Draw() = 0;
+      virtual void Update(float dt) = 0;
+      virtual void Clean() = 0;
+};
+struct Properties
+{
+    public:
+        Properties(string textureID, int x, int y, int width, int height, SDL_RendererFlip flip = SDL_FLIP_NONE)
+        {
+            X = x;
+            Y = y;
+            Flip = flip;
+            Width = width;
+            Height = height;
+            TextureID = textureID;
+        }
+
+    public:
+        string TextureID;
+        int Width, Height;
+        float X, Y;
+        SDL_RendererFlip Flip;
+};
+
+class GameObject : public IObject
+{
+    public:
+        GameObject(Properties* props)
+            : m_TextureID(props->TextureID), m_Width(props->Width), m_Height(props->Height), m_Flip(props->Flip){  m_Transform = new Transform(props->X, props->Y);}
+        virtual void Draw() = 0;
+        virtual void Clean() = 0;
+        virtual void Update(float dt) = 0;
+
+    protected:
+        Transform* m_Transform;
+        string m_TextureID;
+        int m_Width, m_Height;
+        SDL_RendererFlip m_Flip;
+};
+
 #endif
